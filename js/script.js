e788c2f1f84f68486945f7625690fc5ea8463b19
electrobullet <dmitry.pigasin@yandex.ru>
2020-03-29 22:45:28 +0300

    Rewrite script.js and set ru locale

--- js/script.js
+++ js/script.js
@@ -1,59 +1,73 @@
-covidApi("brief", getCovidTable, attrs = { "tableName": "covid-table-global" })
-covidApi("brief", GlobalStatsTable, attrs = { "tableName": "covid-stats-global" })
-covidApi("timeseries?iso2=RU&onlyCountries=true", getCovidTable, attrs = { "tableName": "covid-table-ru", "lines": 5 })
+setGlobalTable("covid-global-table");
+setGlobalStatsTable("covid-global-stats");
+setCountryStatsTable("RU", "covid-ru-stats");
+setCountryTable("RU", "covid-ru-table", 5);
 
-function getGlobalStats(data) {
-    let death = data.deaths / (data.deaths + data.recovered);
-    let recovery = data.recovered / (data.deaths + data.recovered);
-
-    return { "death": death, "recovery": recovery };
-}
-
-function GlobalStatsTable(data, attrs = {}) {
-    let stats = getGlobalStats(data);
+async function setGlobalTable(tableId) {
+    let url = `https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/brief`
+    let response = await fetch(url);
+    data = await response.json();
 
     let html =
         `<tr>
-            <th>Death</th>
-            <th>Recovery</th>
+            <th>Подтверждено</th>
+            <th>Умерли</th>
+            <th>Выздоровели</th>
         </tr>
         <tr>
-            <td>${(stats.death * 100).toFixed(2) + '%'}</td>
-            <td>${(stats.recovery * 100).toFixed(2) + '%'}</td>
-        </tr>`;
+            <td>${data.confirmed}</td>
+            <td>${data.deaths}</td>
+            <td>${data.recovered}</td>
+        </tr>`
 
-    document.getElementById(attrs.tableName).innerHTML = html;
+    document.getElementById(tableId).innerHTML = html;
 }
 
-function getCovidTable(data, attrs = {}) {
-    let info = [];
+async function setGlobalStatsTable(tableId) {
+    let url = `https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/brief`
+    let response = await fetch(url);
+    data = await response.json();
 
     let html =
         `<tr>
-            <th>Date</th>
-            <th>Confirmed</th>
-            <th>Deaths</th>
-            <th>Recovered</th>
+            <th>Смерть</th>
+            <th>Выздоровление</th>
+        </tr>
+        <tr>
+            <td>${(data.deaths / (data.deaths + data.recovered) * 100).toFixed(2) + '%'}</td>
+            <td>${(data.recovered / (data.deaths + data.recovered) * 100).toFixed(2) + '%'}</td>
         </tr>`
 
-    if (data.length == 1) {
-        for (const date in data[0].timeseries) {
-            if (data[0].timeseries.hasOwnProperty(date)) {
-                let element = data[0].timeseries[date];
-                element["date"] = date;
-                info.push(element)
-            }
+    document.getElementById(tableId).innerHTML = html;
+}
+
+async function setCountryTable(iso2, tableId, lines = 0) {
+    let url = `https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/timeseries?iso2=${iso2}&onlyCountries=true`
+    let response = await fetch(url);
+    data = await response.json();
+
+    info = [];
+
+    let html =
+        `<tr>
+            <th>Дата</th>
+            <th>Подтверждено</th>
+            <th>Умерли</th>
+            <th>Выздоровели</th>
+        </tr>`
+
+    for (const date in data[0].timeseries) {
+        if (data[0].timeseries.hasOwnProperty(date)) {
+            let element = data[0].timeseries[date];
+            element["date"] = convertDate(date);
+            info.push(element)
         }
-    }
-    else {
-        info.push(data)
     }
 
     let count = info.length - 1;
-    info[count]["date"] = "Last";
 
-    if (attrs.hasOwnProperty("lines")) {
-        count -= attrs.lines;
+    if (lines != 0) {
+        count -= lines;
     }
     else {
         count = -1;
@@ -70,14 +84,37 @@ function getCovidTable(data, attrs = {}) {
                 <td>${element.recovered}</td>
             </tr>`;
 
-        document.getElementById(attrs.tableName).innerHTML = html;
+        document.getElementById(tableId).innerHTML = html;
     }
 }
 
-async function covidApi(request, func, attrs = {}) {
-    let url = `https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/${request}`
+async function setCountryStatsTable(iso2, tableId) {
+    let url = `https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/timeseries?iso2=${iso2}&onlyCountries=true`
     let response = await fetch(url);
     data = await response.json();
 
-    func(data, attrs);
+    let keys = Object.keys(data[0].timeseries);
+    data = data[0].timeseries[keys[keys.length - 1]];
+
+    let html =
+        `<tr>
+            <th>Смерть</th>
+            <th>Выздоровление</th>
+        </tr>
+        <tr>
+            <td>${(data.deaths / (data.deaths + data.recovered) * 100).toFixed(2) + '%'}</td>
+            <td>${(data.recovered / (data.deaths + data.recovered) * 100).toFixed(2) + '%'}</td>
+        </tr>`
+
+    document.getElementById(tableId).innerHTML = html;
+}
+
+function convertDate(date) {
+    let temp = date.split("/");
+    for (let i = 0; i < temp.length; i++) {
+        if (temp[i].length == 1) {
+            temp[i] = "0" + temp[i];
+        }
+    }
+    return `${temp[1]}.${temp[0]}.${temp[2]}`
 }
